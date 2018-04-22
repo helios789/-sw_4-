3fc6da59b7d65eb5aa0e3c8e9d8edda6c066b8b2
kimhyeongwoo <hwjw9599@dongguk.edu>
2018-04-22 22:20:47 +0900

    server - phaser 연동 및 오류 수정
    기존의 http에서의 express 변경, 발생하는 오류 수정 및 
    phaser 연동  , socket io통신

---
+++ phaser/public/js/client.js
@@ -0,0 +1,14 @@
+var Client = {};
+Client.socket = io.connect();
+
+Client.sendTest = function(){
+    console.log("test sent");
+    Client.socket.emit('test');
+};
+
+
+
+Client.socket.on('newplayer',function(data){
+	console.log(data.id + ' '+data.x+' '+data.y);
+    game_Scene.new_p();
+});
--- phaser/server.js
+++ phaser/server.js
@@ -1,6 +1,13 @@
+var socketio = require('socket.io');
 var express = require("express");
 var path    = require("path");
 var app     = express();
+var fs = require('fs');
+var urlencode = require('urlencode');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname + '/public'))); //Serves resources from public folder
 
@@ -12,11 +19,67 @@ app.use(myLogger)
 
 
 app.get('/',function(req,res){
-	res.sendFile('index.html');
+    res.sendFile('SwPage.html', { root: path.join(__dirname, 'public') });
 });
 
+app.post('/',function(req,res){
+    //쿠키 생성
+    var login = req.body.login;
+    if(login=='1')
+    {
+        res.cookie('auth',true);
+        res.redirect('/game');
+    }
+});
 
+app.get('/game',function(req,res){
+    res.sendFile('index2.html', { root: path.join(__dirname, 'public') });
+});
 
-app.listen(3000, function(){
-	console.log('Server start');
-}); 
+var server = require('http').Server(app);
+
+server.listen(3033,function(){
+    console.log('Server running at http://127.0.0.1:3033');
+});
+
+var roomlist = new Array();
+server.lastPlayderID = 0;
+var io = socketio.listen(server);
+//socket io 연결
+io.sockets.on('connection',function(socket){
+    //room join
+    //사용자 접속시 room join및 접속한 사용자를 room참여 이누언들에게 알립니다.
+    socket.on('join',function() {}unction(data){
+        //socket join을 합니다
+        socket.join(data.roomname);
+        socket.room = data.roomname;
+        //room join인원들에게 메세지를 보냅니다
+        io.sockets.in(socket.room).emit('join',data.userid);
+    });
+    //make room
+    socket.on('make',function(data){
+        roomlist.push(data.roomname);
+        console.log(roomlist);
+        socket.join(data.roomname);
+        socket.room = data.roomname;
+        //room이름을 html로 보낸다.
+        io.sockets.emit('make',{roomlist:roomlist,userid:data.userid});
+    });
+    //message
+    socket.on('message',function(data){
+            io.sockets.in(socket.room).emit('message',{message:data.message,userid:data.userid});
+    });
+    socket.on('test',function(data){
+        console.log('test received');
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: 200,
+            y: 200
+        };
+
+        io.socket.in(socket.room).emit('newplayer',socket.player);
+    });
+
+    socket.on('disconnect',function(){});
+});
+
